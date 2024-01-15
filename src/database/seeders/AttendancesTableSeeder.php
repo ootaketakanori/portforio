9d415a0707c556b46810feb9d3a9de78da1808b4
ootake takanori <mine19876293@gmail.com>
2024-01-15 11:16:42 +0900

    new

--- src/database/seeders/AttendancesTableSeeder.php
+++ src/database/seeders/AttendancesTableSeeder.php
@@ -21,6 +21,12 @@ class AttendancesTableSeeder extends Seeder
             'date' => '2021-03-22',
             'start_time' => '2021-03-22 09:23:00',  // 正しい datetime フォーマット
         ];
-        DB::table('attendances')->insert($param);
-    }
+        DB::table('attendances')->insert([
+            'user_id' => $userId,
+            'date' => now()->toDateString(),
+            'start_time' => now(),
+            // 他のカラム...
+        ]);
+    
+        
 }
